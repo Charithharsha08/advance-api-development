92174da7a91998928e33cfb215920f4800738558
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2024-12-18 08:43:14 +0530

    mapping specification

---
+++ HelloServlet/src/TestServlet.java
@@ -0,0 +1,18 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Get Method triggered");
+        PrintWriter out = resp.getWriter();
+       // out.println("Get Method executed");
+        out.println("<h1>Get Method executed</h1>");
+    }
+}
