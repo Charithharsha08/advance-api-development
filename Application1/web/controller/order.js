8c726d1767ea5c3c571e96153136aaeebd89cf24
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2024-12-26 13:22:05 +0530

    mapping specification

---
+++ Application1/web/controller/order.js
@@ -0,0 +1,12 @@
+var tot;
+
+$("#btnAddToCart").prop("disabled", true);
+
+document.getElementById("btnOrder").addEventListener("click", function(){
+    document.getElementById("customer").style.display="none";
+    // document.getElementById("item").style.display="none";
+    document.getElementById("item").style.display="none";
+    document.getElementById("placeORder").style.display="block";
+    document.getElementById("order").style.display="none";
+
+});
